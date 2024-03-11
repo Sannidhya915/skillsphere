0ffe577dfe62c896e2e83e6b3f9441a774bdfab7
Sannidhya Sahu <sannidhyasahu915@gmail.com>
2024-03-11 17:09:42 +0530

    Payment gateway update

--- server/config/razorpay.js
+++ server/config/razorpay.js
@@ -1,10 +1,16 @@
 const Razorpay = require("razorpay");
+const dotenv = require("dotenv");
 
+// Loading environment variables from .env file
+dotenv.config();
+
+console.log(process.env.RAZOR_KEY);
+console.log(process.env.RAZOR_SECRET);
 exports.instance = new Razorpay({
 	// key_id: process.env.RAZORPAY_KEY,
 	// key_secret: process.env.RAZORPAY_SECRET,
 
 
-	key_id: "a",
-	key_secret: "a",
+	key_id: process.env.RAZOR_KEY,
+	key_secret: process.env.RAZOR_SECRET,
 });
