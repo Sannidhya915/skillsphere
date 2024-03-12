108004df6ad005933d48a7d59b5b0ffcd9b5d543
Sannidhya Sahu <sannidhyasahu915@gmail.com>
2024-03-13 01:56:17 +0530

    Updates for deployment

--- server/config/razorpay.js
+++ server/config/razorpay.js
@@ -4,8 +4,8 @@ const dotenv = require("dotenv");
 // Loading environment variables from .env file
 dotenv.config();
 
-console.log(process.env.RAZOR_KEY);
-console.log(process.env.RAZOR_SECRET);
+// console.log(process.env.RAZOR_KEY);
+// console.log(process.env.RAZOR_SECRET);
 exports.instance = new Razorpay({
 	// key_id: process.env.RAZORPAY_KEY,
 	// key_secret: process.env.RAZORPAY_SECRET,
